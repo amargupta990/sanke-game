0a68246c12b874c9813f1d0e0d7dc6b5a09ddb6d
Amar <ag14290@gmail.com>
2024-09-27 23:48:56 +0530

    first commit

--- anonymus.js
+++
@@ -1,8 +0,0 @@
- let a=function (){
-    console.log("hello...")
-}
-a();
-(function b(){
-    console.log("hello in c")
-})();
-b()
--- callbback.js
+++
@@ -1,9 +0,0 @@
-function fn(a, fn){
-    console.log(a)
-    fn();
-}
-fn(2,hello)
-
-function hello(){
-    console.log("fello in call")
-}
